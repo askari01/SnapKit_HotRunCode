74812bfd10732ce67e275f8f13fe373d053d7e6f
askari01 <farrukh.askari01@gmail.com>
2017-10-12 22:36:17 +0500

    Carthage and hot run code truly awesome

--- SnapkitDemo/ViewController.swift
+++ SnapkitDemo/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var box = UIView()
+    lazy var label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(box)
+        box.snp.makeConstraints { (make) in
+            make
+                .width
+                .height
+                .equalTo(50)
+            make
+                .center
+                .equalTo(self.view)
+        }
+        
+        self.box.addSubview(label)
+        label.text = "hi!"
+        label.snp.makeConstraints { (make) in
+            make
+                .width
+                .height
+                .equalTo(30)
+            make
+                .center
+                .equalTo(box)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension UIViewController {
+    
+    #if DEBUG
+    @objc func injected() {
+        self.view.subviews.flatMap {
+            $0.removeFromSuperview()
+        }
+        viewDidLoad()
+    }
+    #endif
+    
 }
 
